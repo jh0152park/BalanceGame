794d63ad9252c57ca10416f8f13a7375a6bb6b2a
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-16 18:24:11 +0900

    added register modal

--- balance_game/src/screens/Home.tsx
+++ balance_game/src/screens/Home.tsx
@@ -2,10 +2,12 @@ import { Center, Heading, VStack, useDisclosure } from "@chakra-ui/react";
 import { ColorTable } from "../Colors";
 import StartModal from "../components/home/StartModal";
 import { Helmet } from "react-helmet";
+import RegisterModal from "../components/home/RegisterModal";
 
 export default function Home() {
     // const { isOpen, onOpen, onClose } = useDisclosure();
     const startModal = useDisclosure();
+    const registerModal = useDisclosure();
 
     return (
         <>
@@ -44,23 +46,6 @@ export default function Home() {
                                 transform: "scale(1.05)",
                             }}
                             transition="all 0.2s linear"
-                            onClick={startModal.onOpen}
-                        >
-                            시작하기
-                        </Center>
-                        <Center
-                            w="150px"
-                            h="50px"
-                            fontWeight="bold"
-                            fontSize="18px"
-                            borderRadius="10px"
-                            backgroundColor="purple.300"
-                            _hover={{
-                                cursor: "pointer",
-                                bgColor: "purple.500",
-                                transform: "scale(1.05)",
-                            }}
-                            transition="all 0.2s linear"
                         >
                             로그인
                         </Center>
@@ -77,14 +62,36 @@ export default function Home() {
                                 transform: "scale(1.05)",
                             }}
                             transition="all 0.2s linear"
+                            onClick={registerModal.onOpen}
                         >
                             회원가입
+                        </Center>
+                        <Center
+                            w="150px"
+                            h="50px"
+                            fontWeight="bold"
+                            fontSize="18px"
+                            borderRadius="10px"
+                            backgroundColor="purple.300"
+                            _hover={{
+                                cursor: "pointer",
+                                bgColor: "purple.500",
+                                transform: "scale(1.05)",
+                            }}
+                            transition="all 0.2s linear"
+                            onClick={startModal.onOpen}
+                        >
+                            시작하기
                         </Center>
                     </VStack>
 
                     <StartModal
                         isOpen={startModal.isOpen}
                         onClose={startModal.onClose}
+                    />
+                    <RegisterModal
+                        isOpen={registerModal.isOpen}
+                        onClose={registerModal.onClose}
                     />
                 </VStack>
             </Center>
