e1c61f44ae2bb0fd72a4b943d2140c00be367d50
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-16 15:37:56 +0900

    create routers

--- balance_game/src/App.tsx
+++ balance_game/src/App.tsx
@@ -1,11 +1,17 @@
-import { Center } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Center, Heading } from "@chakra-ui/react";
 import React from "react";
+
+const router = createBrowserRouter([]);
 
 function App() {
     return (
-        <Center w="100%" h="100vh">
-            APP
-        </Center>
+        // <Center w="100%" h="100vh">
+        //     <Heading fontFamily="Rubik Burned">
+        //         Balance Game by Instead of me
+        //     </Heading>
+        // </Center>
+        <RouterProvider router={router} />
     );
 }
 
