73638ac5fe83fb27d8ff7d7be60731c5c9fd9146
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-22 20:42:13 +0900

    extra ad

--- balance_game/src/screens/defaults/Layout.tsx
+++ balance_game/src/screens/defaults/Layout.tsx
@@ -8,13 +8,43 @@ export default function Layout() {
     const is_home = location.pathname === "/";
 
     return (
-        <Box w="100%" minH="100vh" p="30px" position={"relative"}>
+        <Box w="100%" minH="125vh" p="30px" position={"relative"}>
             {!is_home && <Header />}
             <Outlet />
-            <Box w={"200px"} position={"absolute"} left={0} top={0} bottom={0}>
+            <Box
+                w={"200px"}
+                position={"absolute"}
+                left={0}
+                top={0}
+                bgColor="blue"
+            >
                 <GoogleAds />
             </Box>
-            <Box w={"200px"} position={"absolute"} right={0} top={0} bottom={0}>
+            <Box
+                w={"200px"}
+                position={"absolute"}
+                left={0}
+                bottom={0}
+                bgColor="green"
+            >
+                <GoogleAds />
+            </Box>
+            <Box
+                w={"200px"}
+                position={"absolute"}
+                right={0}
+                top={0}
+                bgColor="blue"
+            >
+                <GoogleAds />
+            </Box>
+            <Box
+                w={"200px"}
+                position={"absolute"}
+                right={0}
+                bottom={0}
+                bgColor="green"
+            >
                 <GoogleAds />
             </Box>
         </Box>
