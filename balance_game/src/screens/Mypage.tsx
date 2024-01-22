50c3f1a8dec5eeaa4d3b27fc4d19f8fbdab2e1a6
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-22 20:27:11 +0900

    update

--- balance_game/src/screens/Mypage.tsx
+++ balance_game/src/screens/Mypage.tsx
@@ -86,6 +86,7 @@ export default function Mypage() {
                 nickname: "",
                 accessToken: "",
             });
+            setShow("");
             navigage("/");
         },
     });
