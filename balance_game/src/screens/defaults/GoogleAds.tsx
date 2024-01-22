59bd41db9bf877f5e2ee9bb8851e4bf617e4550d
Yuhyeon0516 <rladbgus0516@naver.com>
2024-01-22 15:30:27 +0900

    Added google adsense

--- balance_game/src/screens/defaults/GoogleAds.tsx
+++ balance_game/src/screens/defaults/GoogleAds.tsx
@@ -33,13 +33,15 @@ export default function GoogleAds() {
     }, []);
 
     return (
-        <ins
-            className="adsbygoogle"
-            style={{ display: "block" }}
-            data-ad-client="ca-pub-7748316956330968"
-            data-ad-slot="3545458418"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-        ></ins>
+        <div>
+            <ins
+                className="adsbygoogle"
+                style={{ display: "block" }}
+                data-ad-client="ca-pub-2088200838674495"
+                data-ad-slot="8561726116"
+                data-ad-format="auto"
+                data-full-width-responsive="true"
+            ></ins>
+        </div>
     );
 }
